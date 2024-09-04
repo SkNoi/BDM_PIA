3d39d05df3ca00fe6f01e65e6bb7da05ddf3686a
Bryan Pardo <bpardo0511@hotmail.com>
2024-09-04 16:02:19 -0600

    Cambios y Agregados

---
+++ js/Registro.js
@@ -0,0 +1,17 @@
+function previewImage(event) {
+    const preview = document.getElementById('preview');
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        preview.src = e.target.result;
+        preview.style.display = 'block';
+    };
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = '';
+        preview.style.display = 'none';
+    }
+}
