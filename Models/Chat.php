56c4c27d300df3b99cf76d55f215ee8ffb0f9808
Raphy21 <raphyros12@gmail.com>
2024-12-11 22:57:58 -0600

    cambios en las rutas de send y fetch del chat

--- Models/Chat.php
+++ Models/Chat.php
@@ -46,7 +46,7 @@ if (!isset($_SESSION['ID_User'])) {
 
         // Obtener mensajes cada 1 segundo
         function fetchMessages() {
-            $.get("fetch.php", function (data) {
+            $.get("./fetch.php", function (data) {
                 $("#chat-box").html(data);
                 $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);
             });
@@ -56,7 +56,7 @@ if (!isset($_SESSION['ID_User'])) {
         $("#send-button").click(function () {
             const mensaje = $("#message").val().trim();
             if (mensaje) {
-                $.post("send.php", { usuario_id: usuarioId, mensaje: mensaje }, function () {
+                $.post("./send.php", { usuario_id: usuarioId, mensaje: mensaje }, function () {
                     $("#message").val("");
                     fetchMessages();
                 });
