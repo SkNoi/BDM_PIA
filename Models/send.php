69a6570659a0088823b8aeb8f5fbd6df05ae294b
Raphy21 <raphyros12@gmail.com>
2024-12-11 23:26:43 -0600

    cambios en nombre de variable de send.php

--- Models/send.php
+++ Models/send.php
@@ -2,7 +2,7 @@
 require './config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $usuario_id = $_POST['ID_User'];
+    $usuario_id = $_POST['usuario_id'];
     $mensaje = $_POST['mensaje'];
 
     $stmt = $pdo->prepare("INSERT INTO mensajes (usuario_id, mensaje) VALUES (:usuario_id, :mensaje)");
