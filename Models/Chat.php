054e1a387b29ee075bea6b247a1ce9a242db2015
Raphy21 <raphyros12@gmail.com>
2024-12-11 22:56:58 -0600

    cambios en el estilo del chat

--- Models/Chat.php
+++ Models/Chat.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['ID_User'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat en Línea - Estudiante y Instructor</title>
-    <link rel="stylesheet" href=".css/chat.css">
+    <link rel="stylesheet" href="../css/chat.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
