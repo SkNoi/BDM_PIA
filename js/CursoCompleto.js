d7658bdf18fd57d0626a81acd73e98a38e5e3a6f
SkNoi <bpardo0511@hotmail.com>
2024-12-12 13:23:05 -0600

    Comprobando cusos completos

--- js/CursoCompleto.js
+++ js/CursoCompleto.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function mostrarDetallesCurso(cursoCompleto) {
     console.log(cursoCompleto); 
-    console.log(cursoCompleto.niveles.Video);
+    console.log(cursoCompleto.niveles.Temarios.Video);
 
     document.title = cursoCompleto.curso.Titulo;
 
